6cf3e570d41ed28dc847495eff2b0fa6f1e8b4dd
Raiana Ratti <raianaratti@gmail.com>
2026-08-02 19:56:23 -0400

    Full notes on run.py

--- run.py
+++ run.py
@@ -8,10 +8,23 @@ def resource_path(rel):
     base = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base, rel)
 
+# NOTES ---------------
+#   1. getattr(object, attribute, default) -> gets object.attribute (if existing), else object.default
+#   2. os.path.abspath(__file__) -> gets abs path of file
+#   3. os.path.dirname(__file__) -> gets path to directory in which file sits
+#   4. When Pyinstaller creates a .exe, it unpacks bundled files into temporary folder 
+#           - whose path is stored in sys._MEIPASS
+#   5. os.path.join(base, rel) -> joins base path with relative path to file (given)
+#   6. base becomes path to temporary extracted folder or folder where app file lives
+#           - we return complete path to file
 
 sys.path.insert(0, resource_path("src"))
 from api import Api  # noqa: E402
 
+# NOTES ---------------
+#   1. sys.path -> list of directories that Python searches when you do an import
+#   2. .insert(0, resource_path("src")) -> inserts path to src/ at index 0
+#   3. src contains api.py so now Python can find api and import Api from it
 
 def main():
     api = Api()
@@ -24,6 +37,10 @@ def main():
         min_size=(760, 580),
     )
     webview.start()
+
+# NOTES ---------------
+#   1. webview.create_window -> accepts [title, url of file to load, Python object whose methods you want to expose to JS]
+#   2. The latter is a cool feature as normally, JS cannot directly call Python functions
 
 
 if __name__ == "__main__":
