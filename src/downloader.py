bd1bdff5d9fa13599e72f4c2ce66487c9b84deb3
Raiana Ratti <raianaratti@gmail.com>
2026-08-05 01:18:35 -0400

    Partial notes on downloader.py - unique_path(), download_file()

--- src/downloader.py
+++ src/downloader.py
@@ -35,8 +35,11 @@ def unique_path(directory, filename):
     return candidate
 
 # NOTES --------------
-#   1. 
+#   1. base, ext -> split filename into name + extension
+#   2. candidate -> path to level_one_directory/file
+#   3. while os.path.exists(candidate) -> if following path already exists, keep trying new names to file
 
+# SUMMARY: Finds a filename for a particular file that does not already exist in said directory
 
 def download_file(session, file_obj, dest_dir):
     os.makedirs(dest_dir, exist_ok=True)
@@ -49,3 +52,19 @@ def download_file(session, file_obj, dest_dir):
             for chunk in r.iter_content(8192):
                 f.write(chunk)
     return path
+
+# NOTES ---------------
+#   1. Creates destination directory (if not already existing)
+#   2. Canvas stores two names for a file
+#           - display_name: human-friendly name shown to users in Canvas
+#           - filename: actual filename stored in Canvas
+#      GET request to both types of names (whichever works)
+#   3. Convert the name to the safe_name and create a unique_path to it
+#   4. with session.get(file_obj["url"], stream=True, timeout=60) as r -> Sends GET request to file's download URL
+#           - stream = True means don't download entire file into memory at once, receive it in small pieces instead
+#   5. with open(path, "wb") as f -> Open output file, w = "write" and b = "binary mode" (needed for PDFs, imags, ZIPs, etc.)
+#   6. for chunk in r.iter_content(8192): f.write(chunk) -> Writes info of file 8192 bytes (8 KB) at a time
+#           - More memory efficient than downloading whole file at a time and then writing it
+#   7. return path -> returns path to downlaoded file
+
+# SUMMARY: Downloads a file into provided destination directory and returns path to downloaded file
